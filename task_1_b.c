0b42f237cb914d489a6efee32089da2245988649
nnonne <70632388+nnonne@users.noreply.github.com>
2020-11-28 23:22:08 +0200

    add classwork 13

--- task_1_b.c
+++ task_1_b.c
@@ -25,16 +25,11 @@ int main(){
     FILE *fp1;
     char fname2[20];
     strcpy(fname2,fname);
-    strcat(fname2,(char*)"_lower.txt");
+    strcat(fname2,(char*)"_backwards.txt");
     printf("f = %s",fname2);
     fp1 = fopen(fname2,"wt");
-    for (int i = 0; i < k-1; ++i){
-        if(isupper(mas[i])){
-            fputc(tolower(mas[i]),fp1);
-        }
-        else {
-            fprintf(fp,"%c",mas[i]);
-        }
+    for (int i = k-2; i >= 0; i--){
+        fprintf(fp,"%c",mas[i]);
     }
     fclose(fp1);
 }
--- task_2.c
+++ task_2.c
@@ -1,43 +1,27 @@
 #include <stdio.h>
+#include <ctype.h>
 
-#define N 3
-#define M 3
-
-void print(float matrix[N][M]){
-    int i,j;
-    for (i = 0; i < N; i++){
-        for (j = 0; j < M; j++){
-            printf("%f ",matrix[i][j]);
-    }
-    printf("\n");
-}
-}
-void t(float matr1[N][M], float matr2[N][M]){
-    for (int i = 0; i < N; i++){
-        for (int j = 0; j < M; j++){
-            matr2[j][i] = matr1[i][j];
-        }
-    }
-}
 int main(){
-    float matr[N][N] = {{1.0,2,3},{4,5,6},{7,8,9}};
-    float transp[N][M];
-    int I,J;
-    float a;
-    printf("I = ");
-    scanf("%d",&I);
-    printf("J = ");
-    scanf("%d",&J);
-    printf("A = ");
-    scanf("%f",&a);
-    for (int i = 0; i < N; i++){
-        for (int j = 0; j < M; j++){
-            if ( ( i == I) && (j == J)){
-                matr[i][j] = a;
-            }
+    char * fname = "myfile";
+    FILE *fp;
+    char c;
+    int isdig = 1;
+    int mydig;
+    fp = fopen(fname,"rt");
+    for (int i = 0; i < 2; i++){
+        c = fgetc(fp);
+        if (!isdigit(c)){
+            isdig = 0;
+            //break;
         }
     }
-    t(matr,transp);
-    print(transp);
+    if (isdig){
+        if (c % 2  == 0) printf("digit is even");
+        else printf("digit is odd");
+    }
+    else printf("it is not a digit with two ciphers");
+    fclose(fp);
 
 }
+
+
