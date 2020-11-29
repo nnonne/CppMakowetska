d53f485b9d8fedb4369e0114ea09851aa3ca4636
nnonne <70632388+nnonne@users.noreply.github.com>
2020-11-29 19:59:29 +0200

    add homewotk 14

--- task_1.c
+++ task_1.c
@@ -1,51 +1,31 @@
 #include <stdio.h>
 
-#define N 3
-#define M 3
-int input(int matrix[N][M]){
-    int i,j;
-    for (i = 0; i < N; i++){
-        for (j = 0; j < M; j++){
-            printf("matrix(%d,%d) = ",i,j);
-            scanf("%d",&matrix[i][j]);
-        }
-    }
-    return 0;
-}
-void print(int matrix[N][M]){
-    int i,j;
-    for (i = 0; i < N; i++){
-        for (j = 0; j < M; j++){
-            printf("%d",matrix[i][j]);
-    }
-    printf("\n");
-}
-}
-void t(int matr1[N][M], int matr2[N][M]){
-    int i,j;
-    for (i = 0; i < N; i++){
-        for (j = 0; j < M; j++){
-            matr2[j][i] = matr1[i][j];
-        }
-    }
-}
+typedef struct Book{
+    char cypher;
+    char author;
+    char name;
+}Book;
+
+typedef struct Reader{
+    int number;
+    char surname;
+    int day;
+    int month;
+    int year;
+}Reader;
+
+typedef struct Field{
+    char col;
+    int row;
+}Field;
+
+typedef struct Circle{
+    float radius;
+    float x;
+    float y;
+}Circle;
+
 int main(){
-    int matr[N][N];
-    int transp[N][M];
-    int i,j,n,m;
-    printf("m = ");
-    scanf("%d",&m);
-    printf("n = ");
-    scanf("%d",&n);
-    input(matr);
-    for (i = 0; i < N; i++){
-        for (j = 0; j < M; j++){
-            if (matr[i][j] == m){
-                matr[i][j] = n;
-            }
-        }
-    }
-    t(matr,transp);
-    print(transp);
 
 }
+
