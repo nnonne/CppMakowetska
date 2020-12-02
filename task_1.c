e152f2fcd62858847a47337992e7656bce0b3920
nnonne <70632388+nnonne@users.noreply.github.com>
2020-12-02 21:29:56 +0200

    add homework_15

--- task_1.c
+++ task_1.c
@@ -1,31 +1,124 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct Book{
-    char cypher;
-    char author;
-    char name;
-}Book;
-
-typedef struct Reader{
-    int number;
-    char surname;
-    int day;
-    int month;
-    int year;
-}Reader;
-
-typedef struct Field{
-    char col;
-    int row;
-}Field;
-
-typedef struct Circle{
-    float radius;
-    float x;
-    float y;
-}Circle;
-
+int input(int * arr, size_t n, char * fname){
+    FILE* fp = fopen(fname, "wb");
+    if (fp == NULL){
+        printf("error\n");
+        return -1;
+    }
+    fwrite(arr,n,sizeof(arr[0]),fp);
+    fclose(fp);
+    return EXIT_SUCCESS;
+}
+int output(const char * fname){
+    FILE* fp = fopen(fname, "rb");
+    if (fp == NULL){
+        printf("error\n");
+        return -1;
+    }
+    char res;
+    while (!feof(fp)){
+        int item;
+        res = fread(&item,1,sizeof(item),fp);
+        if (res == 0) break;
+        printf("%d\n",item);
+    }
+    fclose(fp);
+    return EXIT_SUCCESS;
+}
+int append(const int * arr,size_t n, const char * fname){
+    FILE* fp = fopen(fname, "ab");
+    if (fp == NULL){
+        printf("error\n");
+        return -1;
+    }
+    for (size_t i = 0; i < n; i++){
+        fwrite(&arr[i],1,sizeof(arr[i]),fp);
+    }
+    fclose(fp);
+    return EXIT_SUCCESS;
+}
+int del_el(const char * fname,int el){
+    FILE* fp = fopen(fname, "ab");
+    if (fp == NULL){
+        printf("error\n");
+        return -1;
+    }
+    int n = 0;
+    while (!feof(fp)){
+         n++;
+    }
+    char ar[n];
+    char res;
+    int i = 0;
+    while (!feof(fp)){
+        int item;
+        res = fread(&item,1,sizeof(item),fp);
+        if ((int)res != el){
+            ar[i] = res;
+            i++;
+        }
+    }
+    for (size_t i = 0; i < n; i++){
+        fwrite(&ar[i],1,sizeof(ar[i]),fp);
+    }
+    fclose(fp);
+    return EXIT_SUCCESS;
+}
+int in_m(const char * fname,int el){
+    FILE* fp = fopen(fname, "rb");
+    if (fp == NULL){
+        printf("error\n");
+        return -1;
+    }
+    char res;
+    while (!feof(fp)){
+        int item;
+        res = fread(&item,1,sizeof(item),fp);
+        if (res != el){
+            printf("1");
+            fclose(fp);
+            return EXIT_SUCCESS;
+        }
+    }
+    printf("0");
+    fclose(fp);
+    return EXIT_SUCCESS;
+}
+int isempty(const char * fname){
+    FILE* fp = fopen(fname, "ab");
+    if (fp == NULL){
+        printf("error\n");
+        return -1;
+    }
+    int n = 0;
+    while (!feof(fp)){
+        n++;
+    }
+    if (n == 0) return 1;
+    else return 0;
+    fclose(fp);
+}
 int main(){
+    char fname[10] = "myfile";
 
+    int n;
+    printf(" n = ");
+    scanf("%d",&n);
+    int ar[n];
+    for (int i = 0; i < n; ++i){
+        printf("ar[%d] = ",i);
+        scanf("%d",&ar[i]);
+    }
+    input(ar,n,fname);
+    append(ar,n,fname);
+    int a;
+    printf(" input the element you want to check: ");
+    scanf("%d",&a);
+    //del_el(fname,a);
+    in_m(fname,a);
+    //output(fname);
+    printf("is empty : %d", isempty(fname));
 }
 
--- task_6.c
+++ task_6.c
@@ -1,62 +1,80 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 #include <stdlib.h>
 
-int main(){
-    char a[100] = "23+35-23=";
-    int state = 1;
-    int sum = 0, num = 0;
-    for (int i = 0; i < strlen(a); ++i) {
-        if (isdigit(a[i])){
-            num *= 10;
-            char buf[2];
-            buf[0] = a[i];
-            buf[1] = '\0';
-            num += atoi(buf);
-        }
-        else if (a[i] == '+'){
-            if (state == 1){
-                sum += num;
-                num = 0;
-                state = 1;
-            }
-            if (state == 2){
-                sum -= num;
-                num = 0;
-                state = 1;
-            }
-        }
-
-        else if (a[i] == '-'){
-            if (state == 1){
-                sum += num;
-                num = 0;
-                state = 2;
-            }
-            if (state == 2){
-                sum -= num;
-                num = 0;
-                state = 2;
-            }
-        }
-        else if (a[i] == '='){
-            if (state == 1){
-                sum += num;
-                num = 0;
-            }
-            if (state == 2){
-                sum -= num;
-                num = 0;
-
-            }
-            break;
-        }
-        else {
-            printf("wrong string");
-        }
+int input(int * arr, size_t n, char * fname){
+    FILE* fp = fopen(fname, "wb");
+    if (fp == NULL){
+        printf("error\n");
+        return -1;
     }
-    printf("%d",sum);
+    fwrite(arr,n,sizeof(arr[0]),fp);
+    fclose(fp);
+    return EXIT_SUCCESS;
+}
+int output(const char * fname){
+    FILE* fp = fopen(fname, "rb");
+    if (fp == NULL){
+        printf("error\n");
+        return -1;
+    }
+    char res;
+    while (!feof(fp)){
+        int item;
+        res = fread(&item,1,sizeof(item),fp);
+        if (res == 0) break;
+        printf("%d\n",item);
+    }
+    fclose(fp);
+    return EXIT_SUCCESS;
+}
+int evenodd(const char * fname){
+    FILE* fp = fopen(fname, "rb");
+    if (fp == NULL){
+        printf("error\n");
+        return -1;
+    }
+    char res;
+    const char * ev = "G";
+    const char * odd = "H";
+    FILE* fp1 = fopen(ev, "wb");
+    if (fp1 == NULL){
+        printf("error\n");
+        return -1;
+    }
+    FILE* fp2 = fopen(odd, "wb");
+    if (fp2 == NULL){
+        printf("error\n");
+        return -1;
+    }
+    while (!feof(fp)){
+        int item;
+        res = fread(&item,1,sizeof(item),fp);
+        if (res < sizeof(item)) break;
+        if (item % 2 == 0) fwrite(&item,1,sizeof(item),fp1);
+        else fwrite(&item,1,sizeof(item),fp2);
+    }
+    fclose(fp);
+    fclose(fp1);
+    printf("even numbers: \n");
+    output(ev);
+    printf("odd numbers: \n");
+    fclose(fp2);
+    output(odd);
+    return EXIT_SUCCESS;
 }
 
+int main(){
+    char fname[10] = "myfile";
+
+    int n;
+    printf(" n = ");
+    scanf("%d",&n);
+    int ar[n];
+    for (int i = 0; i < n; ++i){
+        printf("ar[%d] = ",i);
+        scanf("%d",&ar[i]);
+    }
+    input(ar,n,fname);
+    evenodd(fname);
+}
 
