7f907c51fc75e2cb4e78a5fbad35f9028f780353
nnonne <70632388+nnonne@users.noreply.github.com>
2020-12-04 18:52:58 +0200

    add homework_17

--- hw_task_19.cpp
+++ hw_task_19.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+#include <fstream>
 
+int isin(std :: string w,std :: string s){
+    for (int i = 0; i < w.size(); i++){
+        //
+        if (s.find(w[i]) == -1){
+            return 0;
+        }
+    }
+    return 1;
+}
+int main() {
+    std :: string word, symb;
+    std :: cout << "input your word: ";
+    std :: cin >> word;
+    std :: cout << "input symbols to check: ";
+    std :: cin >> symb;
+    std :: cout << isin(word,symb);
+
+}
